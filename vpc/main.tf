4d9b3918e19f5abdd4a2164c043abc7437ae90a8
Chowdary-Hari <chandrahari321@outlook.com>
2025-09-13 15:22:07 +0530

    Automated commit #1569

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,6 +1,8 @@
 module "vpc" {
   source = "git::https://github.com/wavedevops/terraform-modules.git"
+  for_each   = var.vpc
   cidr_block = var.cidr_block
   tags             = local.tags
   env              = var.env
+  subnets    = each.value["subnets"]
 }
